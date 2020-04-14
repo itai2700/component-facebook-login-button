dc58c0fedcf9dbe37b8d889c0ea77992dae3e84e
itai2700 <39342966+itai2700@users.noreply.github.com>
2020-04-14 10:38:11 +0300

    completed project

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Facebook from '../Facebook/Facebook';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+      <h1>Facebook Auth Practice</h1>
+      <h2>To Get Started, Authenticate With Facebook</h2>
+      <Facebook />
+    </div>
+  );
+}
+
+export default App;
